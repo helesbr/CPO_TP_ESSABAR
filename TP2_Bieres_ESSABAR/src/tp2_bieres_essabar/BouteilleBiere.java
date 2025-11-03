ce68b11bfeadcfd0bae03bebc9a5556907e17f64
Hélias Essabar <helias.essabar@epfedu.fr>
2025-11-03 15:04:31 +0100

    TP2 1) finie

--- TP2_Bieres_ESSABAR/src/tp2_bieres_essabar/BouteilleBiere.java
+++ TP2_Bieres_ESSABAR/src/tp2_bieres_essabar/BouteilleBiere.java
@@ -9,13 +9,38 @@ package tp2_bieres_essabar;
  * @author helia
  */
 public class BouteilleBiere {
+
     String nom;
     double degreAlcool;
     String brasserie;
     boolean ouverte;
-    
-    public void lireEtiquette(){
-            
-    System.out.println("Bouteille de " + nom +" (" + degreAlcool + " degres) \nBrasserie : " + brasserie ) ;
+@Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = nom + " (" + degreAlcool + " degres) Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
 }
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+
+    public void lireEtiquette() {
+
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degres) \nBrasserie : " + brasserie);
+    }
+
+    public boolean Decapsuler() {
+        if (!ouverte) {
+            ouverte = true;
+            return true;
+        } else {
+            System.out.print("biere deja ouverte");
+            return false;
+        }
+    }
 }
--- TP2_Bieres_ESSABAR/src/tp2_bieres_essabar/TP2_Bieres_ESSABAR.java
+++ TP2_Bieres_ESSABAR/src/tp2_bieres_essabar/TP2_Bieres_ESSABAR.java
@@ -14,12 +14,33 @@ public class TP2_Bieres_ESSABAR {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere();
-        uneBiere.nom = "cuvée des trolls";
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls", 7.0, "Dubuisson");
+        uneBiere.nom = "cuvee des trolls";
         uneBiere.degreAlcool = 7.0;
         uneBiere.brasserie = "Dubuisson";
-
+        uneBiere.ouverte = false;
         uneBiere.lireEtiquette();
+    
+    BouteilleBiere Leffe = new BouteilleBiere("Leffe", 6.6,"Abbaye de Leffe") ;
+    Leffe.lireEtiquette();
+    if(Leffe.Decapsuler()==true){
+        System.out.println("La biere a ete ouverte");
     }
-
+    
+    BouteilleBiere Heineken = new BouteilleBiere("Heineken", 8.3, "Pays-Bas");
+    Heineken.lireEtiquette();
+    
+    BouteilleBiere Guinness = new BouteilleBiere("Guinness", 5.2, "Irlande");
+    Guinness.lireEtiquette();
+    if(Guinness.Decapsuler()==true){
+        System.out.println("La biere a ete ouverte");
+    }
+    System.out.println(uneBiere);
+    System.out.println(Guinness);
+    }                                                                                       
+    
 }
+        
+        
+
+
